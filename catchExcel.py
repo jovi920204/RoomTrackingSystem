2ba6709a119bdd0359326f008a2e62fc21bbf213
jovi <73748197+jovi920204@users.noreply.github.com>
2022-08-13 23:15:40 +0800

    commit

--- catchExcel.py
+++ catchExcel.py
@@ -18,26 +18,36 @@ class GoogleSheets(GoogleAPIClient):
         )
         response = request.execute()
         return response
-
-room = '10202'
-bed = '2'
+inputText = '201-4'
+room = ''
+bed = ''
+floor = ''
+newText = inputText.split('-')
+room = newText[0]
+if (len(room) == 3):
+    floor = room[0:1]
+    room = '10'+room
+elif (len(room) == 4):
+    floor = room[0:2]
+    room = '1'+room
+bed = newText[1]
 
 if __name__ == '__main__':
     myWorksheet = GoogleSheets()
-    # pprint(myWorksheet.getWorksheet(
-    #     spreadsheetId='1-Z71G5IrXe3oUCXqHVSao_lqksUy7u6Hi07YdoTKHZA',
-    #     range='2F'
-    # )['values'])
-    floorSituation = myWorksheet.getWorksheet(
-        spreadsheetId='1-Z71G5IrXe3oUCXqHVSao_lqksUy7u6Hi07YdoTKHZA',
-        range='2F'
-    )['values']
-    df = pd.DataFrame(floorSituation)
-    df = df[df[0]==room]
-    df = df[df[1]==bed]
-    ret = df.iloc[0,2]
-    if ret == 'FALSE':
-        print("有空位")
-    else:
-        print("沒空位")
-
+    try:
+        floorSituation = myWorksheet.getWorksheet(
+            spreadsheetId='1-Z71G5IrXe3oUCXqHVSao_lqksUy7u6Hi07YdoTKHZA',
+            range = floor+'F'
+        )['values']
+        df = pd.DataFrame(floorSituation)
+        df = df[df[0]==room]
+        df = df[df[1]==bed]
+        ret = df.iloc[0,2]
+        print("房號："+room+'-'+bed)
+        if ret == 'TRUE':
+            print("床位已空，可以入住")
+        elif ret == 'FALSE':
+            print("尚未退宿，請再等等")
+    except:
+        print("房號："+room+'-'+bed)
+        print("查無此房")
